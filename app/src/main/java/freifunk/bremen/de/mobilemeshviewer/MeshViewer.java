db639aace18ddd319ec927d87439db0a7207eb85
He1md4ll <martin.suckau@web.de>
2016-03-29 22:57:26 +0200

    added ViewPager for fragment selection

--- app/src/main/java/freifunk/bremen/de/mobilemeshviewer/MeshViewer.java
+++ app/src/main/java/freifunk/bremen/de/mobilemeshviewer/MeshViewer.java
@@ -1,12 +1,21 @@
 package freifunk.bremen.de.mobilemeshviewer;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
@@ -17,11 +26,16 @@ public class MeshViewer extends RoboActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @InjectView(R.id.toolbar)
-    Toolbar toolbar;
+    private Toolbar toolbar;
     @InjectView(R.id.drawer_layout)
-    DrawerLayout drawer;
+    private DrawerLayout drawer;
     @InjectView(R.id.nav_view)
-    NavigationView navigationView;
+    private NavigationView navigationView;
+    @InjectView(R.id.container)
+    private ViewPager viewPager;
+    @InjectView(R.id.tabs)
+    private TabLayout tabLayout;
+    private SectionsPagerAdapter sectionsPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +48,11 @@ public class MeshViewer extends RoboActivity
                 return MeshViewer.this.onActionBarItemSelected(item);
             }
         });
+
+        sectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+        viewPager.setAdapter(sectionsPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -90,5 +109,77 @@ public class MeshViewer extends RoboActivity
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
     }
 }
