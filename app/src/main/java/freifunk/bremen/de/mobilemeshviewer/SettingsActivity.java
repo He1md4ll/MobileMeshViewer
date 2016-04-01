dc74003bed68bd044fee241541afc8740be65292
anon6789 <anon6789@crater.uberspace.de>
2016-04-01 13:27:20 +0200

    fixed error in example code due to clean up

--- app/src/main/java/freifunk/bremen/de/mobilemeshviewer/SettingsActivity.java
+++ app/src/main/java/freifunk/bremen/de/mobilemeshviewer/SettingsActivity.java
@@ -61,7 +61,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 // using RingtoneManager.
                 if (TextUtils.isEmpty(stringValue)) {
                     // Empty values correspond to 'silent' (no ringtone).
-                    preference.setSummary(R.string.pref_ringtone_silent);
 
                 } else {
                     Ringtone ringtone = RingtoneManager.getRingtone(
