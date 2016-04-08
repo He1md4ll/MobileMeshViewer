4f3fc3ec982e4273095cc6802c43642d7eaac852
anon6789 <anon6789@crater.uberspace.de>
2016-04-08 16:35:31 +0200

    added REST Service for notes.json

--- app/src/main/java/freifunk/bremen/de/mobilemeshviewer/api/FreifunkRestConsumer.java
+++ app/src/main/java/freifunk/bremen/de/mobilemeshviewer/api/FreifunkRestConsumer.java
@@ -1,5 +1,6 @@
 package freifunk.bremen.de.mobilemeshviewer.api;
 
+import freifunk.bremen.de.mobilemeshviewer.node.model.simple.NodeDetailList;
 import freifunk.bremen.de.mobilemeshviewer.node.model.simple.NodeList;
 import retrofit.Call;
 import retrofit.http.GET;
@@ -8,4 +9,7 @@ public interface FreifunkRestConsumer {
 
     @GET("data/nodelist.json")
     Call<NodeList> getNodeList();
+
+    @GET("data/nodes.json")
+    Call<NodeDetailList> getNodeDetailList();
 }
