c3e14825fe474c30ea9360d765fd3954a8abd213
anon6789 <anon6789@crater.uberspace.de>
2016-04-08 18:04:36 +0200

    Revert "added REST service for nodes.json"
    This reverts commit b8b78ee856f473a6f1d13ee1d5c126cb41b0ed02.

--- app/src/main/java/freifunk/bremen/de/mobilemeshviewer/api/FreifunkRestConsumer.java
+++ app/src/main/java/freifunk/bremen/de/mobilemeshviewer/api/FreifunkRestConsumer.java
@@ -1,6 +1,5 @@
 package freifunk.bremen.de.mobilemeshviewer.api;
 
-import freifunk.bremen.de.mobilemeshviewer.node.model.detail.NodeDetailList;
 import freifunk.bremen.de.mobilemeshviewer.node.model.simple.NodeList;
 import retrofit.Call;
 import retrofit.http.GET;
@@ -9,8 +8,4 @@ public interface FreifunkRestConsumer {
 
     @GET("data/nodelist.json")
     Call<NodeList> getNodeList();
-
-
-    @GET("data/nodes.json")
-    Call<NodeDetailList> getNodeDetailList();
 }
